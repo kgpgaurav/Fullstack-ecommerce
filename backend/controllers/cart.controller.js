f113240c2a2b97331b1b674924a0c50501e144a3
Adhiraj <adhirajgu22@iitk.ac.in>
2025-05-14 18:35:33 +0530

    verified the cart route and controllers

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,30 +1,28 @@
 import Product from "../models/product.model.js"; 
 
-
-export const getCartProducts= async (req,res)=>{
+export const getCartProducts = async (req,res)=>{
     try{
-        const products= await Product.find({
+        const products = await Product.find({
             _id:{
                 $in:req.user.cartItems
             }
         })
-
+        //add quantity to the product object
         const cartItems=products.map((product)=>{
             const item=req.user.cart.find((cartItem)=>cartItem.id===product.id);
             return{...product.toJSON(),quantity:item.quantity}
-    })
-    res.json(cartItems);
-}catch(error){
-    console.log("Error in getCartProducts controller",error.message);
-    res.status(500).json({message:"server error",error:error.message});
+        })
+        res.json(cartItems);
+    } catch(error){
+        console.log("Error in getCartProducts controller",error.message);
+        res.status(500).json({message:"server error",error:error.message});
+    }
 }
-}
-
 
 export const addToCart = async (req, res) => {
     try {
         const { productId } = req.body;
-        const user = req.user;
+        const user = req.user; //the route was protected and we have send req.user = user in middleware so can get this here
 
         const existingItem = user.cart.find((item) => item.id === productId);
         if (existingItem) {
@@ -37,7 +35,7 @@ export const addToCart = async (req, res) => {
         res.json(user.cartItems);
     } catch (error) {
         console.log("Error in addToCart controller", error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500).json({ message: "server error", error: error.message });
     }
 }
 
@@ -49,10 +47,8 @@ export const removeAllFromCart = async (req, res) => {
         if (!productId) {
             user.cartItems = [];
         } else {
-            user.cartItems = user.cartItems.filter((item) => item !== productId);
+            user.cartItems = user.cartItems.filter((item) => item !== productId);  //filtering out the product from the cart array 
         }
-
-
         await user.save();
         res.json(user.cartItems);
     } catch (error) {
@@ -67,17 +63,16 @@ export const updateQuantity = async (req, res) => {
         const { quantity } = req.body;
 
         const user = req.user;
-
         const existingItem = user.cartItems.find((item) => item.id === productId);
+
         if (existingItem) {
             if (quantity === 0) {
-                user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+                user.cartItems = user.cartItems.filter((item) => item.id !== productId); //if qt is 0 just filter out the product from the cart items
                 await user.save();
                 return res.json(user.cartItems);
             }
             existingItem.quantity = quantity;
             await user.save();
-
             res.json(user.cartItems);
         }else{
             res.status(404).json({message:"Item not found in cart"});
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
-// import cartRoutes from './routes/cart.route.js';
+import cartRoutes from './routes/cart.route.js';
 // import couponRoutes from './routes/coupon.route.js';
 // import paymentRoutes from './routes/payment.route.js';
 // import analyticsRoutes from './routes/analytics.route.js';
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes); 
 app.use("/api/product", productRoutes);
-// app.use("/api/cart",cartRoutes);
+app.use("/api/cart",cartRoutes);
 // app.use("/api/coupon",couponRoutes);
 // app.use("/api/payments",paymentRoutes);
 // app.use("api/analytics",analyticsRoutes);
